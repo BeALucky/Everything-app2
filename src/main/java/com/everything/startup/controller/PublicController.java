61394f059134ea33057dc72a23edcb088e382201
BeALucky <bealucky.dev@gmail.com>
2024-08-25 17:11:44 +0530

    first

---
+++ src/main/java/com/everything/startup/controller/PublicController.java
@@ -0,0 +1,20 @@
+package com.everything.startup.controller;
+
+//import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+//@Slf4j
+public class PublicController {
+
+    @GetMapping("health-check")
+    public ResponseEntity<String> healthCheck(){
+//        log.info("Health Check Started");
+        return ResponseEntity.ok("OK");
+
+    }
+}
